65b9bbc6abfa7f04cc6acbfeac9cc63cb7488744
Hirotada Fujita <c0b2212931@edu.teu.ac.jp>
2023-09-26 16:12:29 +0900

    練習問題６

--- pygame_image.py
+++ pygame_image.py
@@ -7,19 +7,25 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
+    bg_img2 = pg.transform.flip(bg_img,True,False)
     kt_img = pg.image.load("fig/3.png")
     kt_img = pg.transform.flip(kt_img, True, False)
     kt_img3 = pg.transform.rotate(kt_img, 10)
-    kt_imgs = [kt_img, kt_img3]
+    kt_img4 = pg.transform.rotate(kt_img, 11)
+    kt_img5 = pg.transform.rotate(kt_img, 12)
+    kt_img6 = pg.transform.rotate(kt_img, 13)
+    kt_imgs = [kt_img, kt_img3, kt_img4, kt_img5, kt_img6]
 
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        screen.blit(bg_img, [0, 0])
+        x = tmr%1600
+        screen.blit(bg_img,[-x, 0])
+        screen.blit(bg_img2, [1600-x, 0])
         
-        screen.blit(kt_imgs[tmr%2],[300,200])
+        screen.blit(kt_imgs[tmr%5],[300,200])
 
         pg.display.update()
         tmr += 1        
